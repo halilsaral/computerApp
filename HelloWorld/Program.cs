b62e05fea028e0ee9f088ad9a79daca43c5ffe29
Halil İbrahim Saral <hisaral@Halil-Air.home>
2023-02-28 01:19:36 +0300

    second

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -5,25 +5,13 @@ namespace HelloWorld
 {  
     public class Computer
     {
-        public string Motherboard { get; set; }
+        public string Motherboard { get; set; } = "";
         public int CPUCores { get; set; }
         public bool HasWifi { get; set; }
         public bool HasLTE { get; set; }
         public DateTime ReleaseDate { get; set; }
         public decimal Price { get; set; }
-        public string VideoCard { get; set; }
-
-        public Computer()
-        {
-            if (VideoCard == null)
-            {
-                VideoCard = "";
-            }
-            if (Motherboard == null)
-            {
-                Motherboard = "";
-            }
-        }
+        public string VideoCard { get; set; } = "";
     } 
 
     internal class Program
