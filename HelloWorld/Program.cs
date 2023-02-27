ac701fdf6dd875f3bfb8f7f95fbaf67a3668eee2
Halil İbrahim Saral <hisaral@Halil-Air.home>
2023-02-28 01:14:23 +0300

    done

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace HelloWorld
+{  
+    public class Computer
+    {
+        public string Motherboard { get; set; }
+        public int CPUCores { get; set; }
+        public bool HasWifi { get; set; }
+        public bool HasLTE { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public decimal Price { get; set; }
+        public string VideoCard { get; set; }
+
+        public Computer()
+        {
+            if (VideoCard == null)
+            {
+                VideoCard = "";
+            }
+            if (Motherboard == null)
+            {
+                Motherboard = "";
+            }
+        }
+    } 
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Computer myComputer = new Computer()
+            {
+                Motherboard = "Asus",
+                CPUCores = 4,
+                HasWifi = true,
+                HasLTE = false,
+                ReleaseDate = DateTime.Now,
+                Price = 954.43m,
+                VideoCard = "Nvidia"
+            };
+            Console.WriteLine(myComputer.Motherboard);
+            Console.WriteLine(myComputer.ReleaseDate);
+            Console.WriteLine(myComputer.Price);
+        }
+    }
+}
